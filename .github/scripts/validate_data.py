792ddfe45ffd76dcad281c611f0d761b086cd536
DustyRoller <DustyRoller@github.com>
2026-01-23 22:34:31 +0000

    Add action to validate data files

---
+++ .github/scripts/validate_data.py
@@ -0,0 +1,36 @@
+import json
+import locale
+import unicodedata
+from pathlib import Path
+
+locale.setlocale(locale.LC_COLLATE, 'en_US.UTF-8')
+
+
+def normalise_string(string: str) -> str:
+    # Normalise any unicode characters in the string to
+    # allow alphabetical sorting to work.
+    return ''.join(c for c in unicodedata.normalize('NFD', string) if unicodedata.category(c) != 'Mn')
+
+
+# Loop through all of the files in the data directory.
+# Note that this script will be run from the root directory.
+files = Path("data/").glob("*.json")
+
+for file in files:
+    print(f"Validating {file.name}")
+    with Path(file).open(encoding='utf-8') as f:
+        data: list[str] = json.load(f)
+
+        # Make sure the file doesn't include any duplicate values.
+        if len(data) != len(set(data)):
+            exit(f"{file} contains duplicate entries")
+
+        # Make sure the values are in alphabetical order.
+        sorted_data: list[str] = sorted(data, key=normalise_string)
+
+        if data != sorted_data:
+            for i, d in enumerate(data):
+                if data[i] != sorted_data[i]:
+                    print(data[i])
+                    print(sorted_data[i])
+            exit(f"{file} is not in alphabetical order")
